2eebff5be84a350792457c05e5757f7e7e4bc827
Максим Ефимов <efimov_m@t.top-academy.ru>
2024-02-26 21:22:32 +0300

    Добавьте файлы проекта.

---
+++ CppListSingleProject/CppListSingleProject.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+
+template <typename T>
+struct Node
+{
+    T value;
+    Node<T>* next = nullptr;
+};
+
+template <typename T>
+void PushHead(Node<T>*& head, T value);
+
+template <typename T>
+void List(Node<T>* head);
+
+template <typename T>
+T GetValue(Node<T>* head, int index);
+
+
+int main()
+{
+    Node<int>* head = nullptr;
+
+    PushHead(head, 100);
+    PushHead(head, 200);
+    PushHead(head, 300);
+    PushHead(head, 400);
+    PushHead(head, 500);
+    PushHead(head, 600);
+
+    List(head);
+
+    std::cout << "\n" << GetValue(head, 0) << "\n";
+}
+
+template<typename T>
+void PushHead(Node<T>*& head, T value)
+{
+    Node<T>* nodeNew{ new Node<T> };
+
+    nodeNew->next = head;
+    nodeNew->value = value;
+
+    head = nodeNew;
+}
+
+template<typename T>
+void List(Node<T>* head)
+{
+    while (head)
+    {
+        std::cout << head->value << "\n";
+        head = head->next;
+    }
+}
+
+template<typename T>
+T GetValue(Node<T>* head, int index)
+{
+    for (int i{}; i < index; i++)
+        head = head->next;
+    return head->value;
+}
